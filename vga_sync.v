38211105224dbd4a6e42ae53e99c9f6a5b3961a4
Rodrigo <rodrigof0501@gmail.com>
2024-08-20 18:50:26 -0300

    create buffers and refresh buffers with 50 MHz

--- vga_sync.v
+++ vga_sync.v
@@ -1,32 +1,60 @@
-module interface (CLO, Reset, v_sync, h_sync, blank, sx, sy); // ClockOut RGB, R, G, B,
+module vga_sync  (clock50, clock25, reset, v_sync, h_sync, blank, pixel_x, pixel_y); // ClockOut RGB, R, G, B,
 
-	input CLK25, Reset;
-	// output [7:0] VGA_R, VGA_G, VGA_B;
-	output v_sync, h_sync, blank;
-	output reg [9:0] sx, sy;
+	input wire clock50, clock25, reset;
+	output wire v_sync, h_sync, blank;
+	output wire [9:0] pixel_x, pixel_y;
 	
+	// Sync buffers
+	reg hs_count, vs_count; 
+	wire hs_next, vs_next;
 	
-	parameter HA_END = 639,
-			  HS_STA = HA_END + 16,
-			  HS_END = HS_STA + 96,
-			  LINE = 799;
-	
-	parameter VA_END = 479,
-			  VS_STA = VA_END + 10,
-			  VS_END = VS_STA + 2,
-			  SCREEN = 524;
+	// Position buffers
+	reg [9:0] pixel_x_count, pixel_x_next; 
+	reg [9:0] pixel_y_count, pixel_y_next;
 
-	always @(posedge CLK25) begin
-			if (sx == LINE) begin
-				sx <= 0;
-				sy <= (sy == SCREEN) ? 0: sy + 1;
+
+	parameter HA_END = 10'd640, // Active area 
+			  HS_STA = HA_END + 10'd16, // Front-porch
+			  HS_END = HS_STA + 10'd96, // Sync
+			  LINE = 10'd799; // ... + Back_porch
+	
+	parameter VA_END = 10'd480, // Active area
+			  VS_STA = VA_END + 10'd10, // Front-porch
+			  VS_END = VS_STA + 10'd2, // Sync
+			  SCREEN = 10'd524; // ... + Back-porch
+
+	always @(posedge clock50 or negedge reset) begin
+		if (!reset) begin
+			hs_count <= 1'b0;
+			vs_count <= 1'b0;
+			pixel_x_count <= 10'd0;
+			pixel_y_count <= 10'd0;
+		end else begin
+			hs_count <= hs_next;
+			vs_count <= vs_next;
+			pixel_x_count <= pixel_x_next;
+			pixel_y_count <= pixel_y_next;
+		end
+	end
+
+	always @(posedge clock25) begin
+			if (pixel_x_count == LINE) begin
+				pixel_x_next <= 10'd0;
+				pixel_y_next <= (pixel_y_count == SCREEN) ? 10'd0:pixel_y_count + 10'd1;
 			end else begin
-				sx <= sx + 1;
+				pixel_x_next <= pixel_x_count + 10'd1;
 			end
 		end
 
 
-	assign blank = ((sx <= HA_END) && (sy <= VS_END))? 1:0; //639 491
-	assign h_sync = ((sx >= HS_STA) && (sx < HS_END))? 0:1; //
-	assign v_sync = ((sy >= VS_STA) && (sy < VS_END))? 0:1;
+	assign hs_next = ((pixel_x_count >= HS_STA) && (pixel_x_count < HS_END)) ? 1'b0:1'b1; 
+	assign vs_next = ((pixel_y_count >= VS_STA) && (pixel_y_count < VS_END)) ? 1'b0:1'b1;
+
+	// outputs
+	assign blank = ((pixel_x_count < HA_END) && (pixel_y_count < VA_END)) ? 1'b1:1'b0; 
+	assign h_sync = hs_count;
+	assign v_sync = vs_count;
+	assign pixel_x = pixel_x_count;
+	assign pixel_y = pixel_y_count;
+
 endmodule
